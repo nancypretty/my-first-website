c1a056f3f12ead4b18c8e19942f76994ba717562
nancypretty <nancypgenia@gmail.com>
2023-02-27 19:04:52 +0700

    fixing ejs file render and layout

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ const ejs = require("ejs");
 
 
 const app = express();
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.set("view engine", "ejs");
 
+
+app.get("/", function(req, res) {
+    res.render("home");
+})
+
+app.get("/project", function(req, res) {
+    res.render("project");
+})
+
+app.get("/about", function(req, res) {
+    res.render("about");
+})
 
 
 
